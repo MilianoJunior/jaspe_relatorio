1afbc2edbd3c59fefd797eb2f8917b51f1b48a8f
MilianoJunior <jrmfilho37@gmail.com>
2024-10-21 13:51:53 -0300

    atualização AD

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ import random
 
 from streamlit import session_state
 
+cont = 0
 
+def tempo_wrapper(func):
+    # @st.cache(allow_output_mutation=True)
+    def wrapper(*args, **kwargs):
+        global cont
+        cont += 1
+        print(f' {cont} - name: {func.__name__}')
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Tempo de execução: {end_time - start_time:.2f} segundos")
+        print('____'*10)
+        return result
+    return wrapper
+
+@tempo_wrapper
 def main():
     # Configurar a página do Streamlit
     st.set_page_config(layout="wide", page_icon="logo.png")
@@ -44,7 +60,7 @@ def main():
 
     # st.session_state['dados'] = True
 
-
+@tempo_wrapper
 def exibir_cabecalho():
     # Exibir o cabeçalho da página com o logo e o título
     col1, col2 = st.columns([1, 5])
@@ -65,21 +81,22 @@ def exibir_cabecalho():
     info_str = " | ".join([f"{key} {value}" for key, value in info.items()])
     st.write(info_str)
 
-
+@st.cache_data
+@tempo_wrapper
 def carregar_dados():
     # Leitura do arquivo CSV diretamente da pasta
     df = pd.read_csv("ug01_1.csv", sep=";")
     df['data_hora'] = pd.to_datetime(df['data_hora'], errors='coerce')
     return df
 
-
+@tempo_wrapper
 def selecionar_colunas(df):
     # Criar um seletor para as colunas do DataFrame
     st.write("#### Dados para UG-01:")
     columns = df.columns.tolist()
     return st.multiselect("Selecione até 3 colunas para plotar em relação a data_hora:", columns, default=columns[:3])
 
-
+@tempo_wrapper
 def selecionar_intervalo_simulacao(df):
     # Criar a linha de seleção de datas, horas e simulação
     col1, col2, col3, col4 = st.columns([2, 2, 2, 2])
@@ -104,12 +121,13 @@ def selecionar_intervalo_simulacao(df):
     return start_datetime, end_datetime
 
 
-@st.cache_data
+@tempo_wrapper
 def carregar_dados_filtrados(df, start_datetime, end_datetime):
     # Função para carregar dados em cache
     mask = (df.index >= start_datetime) & (df.index <= end_datetime)
     return df.loc[mask]
 
+@tempo_wrapper
 def mostrar_dados(df):
     # Converter o DataFrame para CSV
     csv = df.to_csv(index=False)
@@ -122,13 +140,11 @@ def mostrar_dados(df):
         mime="text/csv"
     )
 
-
-
 def gerar_cor_aleatoria():
     # Gera uma cor aleatória em formato hexadecimal
     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
-
+@tempo_wrapper
 def plotar_graficos(df, selected_columns):
     # Lista de cores padrão
     cores_padrao = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
@@ -137,33 +153,28 @@ def plotar_graficos(df, selected_columns):
     # Plotar o gráfico das colunas selecionadas em relação à data_hora usando Plotly
     if selected_columns:
         for i, column in enumerate(selected_columns):
-            col_limite, col_grafico = st.columns([1, 15])
+            col_limite, col_grafico = st.columns([1, 19])
 
             with col_limite:
-                max_val = st.number_input(f"Máx. para {column}", value=float(df[column].max()), key=f"max_{column}")
-                min_val = st.number_input(f"Mín. para {column}", value=float(df[column].min()), key=f"min_{column}")
-                cor_grafico = st.color_picker(f"Cor {column}", value=random.choice(cores_padrao), key=f"color_{column}")
+                max_val = st.number_input(f"Máx.", value=float(df[column].max()), key=f"max_{column}")
+                min_val = st.number_input(f"Mín.", value=float(df[column].min()), key=f"min_{column}")
+                cor_grafico = st.color_picker(f"Cor", value=cores_padrao[i], key=f"color_{column}")
 
             with col_grafico:
                 fig = go.Figure()
                 fig.add_trace(go.Scatter(x=df.index, y=df[column], mode='lines', name=column, line=dict(color=cor_grafico)))
                 fig.update_yaxes(range=[min_val, max_val])
-                fig.update_layout(title=f"{column} vs Data e Hora", height=300)
+                fig.update_layout(title=f"{column} vs Data e Hora", height=500)
                 st.plotly_chart(fig)
+    print("\n================================")
 
-
-# def mostrar_dados(df):
-#     # Mostrar o DataFrame no Streamlit
-#     st.write("#### Dados:")
-#     st.dataframe(df)
-
-
+@tempo_wrapper
 def mostrar_estatisticas(df):
     # Mostrar estatísticas descritivas
     st.write("#### Estatísticas Descritivas:")
     st.write(df.describe())
 
-
+@tempo_wrapper
 def exibir_informacoes_empresa():
     # Exibir informações da empresa
     engesep = {
@@ -174,7 +185,7 @@ def exibir_informacoes_empresa():
     engesep_str = " | ".join([f"{key}: {value}" for key, value in engesep.items()])
     st.write(engesep_str)
 
-
+@tempo_wrapper
 def filtrar_dados(df):
     # Filtrar colunas e processar DataFrame
     colunas = ['data_hora', 'tensao_fase_A', 'tensao_fase_B', 'tensao_fase_C', 'corrente_fase_A',
@@ -196,6 +207,14 @@ def filtrar_dados(df):
 
 if __name__ == "__main__":
     main()
+
+'''
+
+
+'''
+
+
+
 
 # import pandas as pd
 # import streamlit as st
